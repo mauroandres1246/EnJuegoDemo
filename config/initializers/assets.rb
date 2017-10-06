16500ea706faf25d22f238c5dfd8859c86d9f849
Mauro <mauroandres1246@gmail.com>
2017-10-06 16:25:35 -0600

    se ah agregado el javascript a la aplicacion

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,19 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( vendors/jquery/jquery.min.js )
+Rails.application.config.assets.precompile += %w( vendors/wow.min.js )
+Rails.application.config.assets.precompile += %w( vendors/bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( vendors/flexslider/jquery.flexslider-min.js )
+Rails.application.config.assets.precompile += %w( vendors/jquery.countTo.js )
+Rails.application.config.assets.precompile += %w( vendors/jquery.bxslider.min.js )
+Rails.application.config.assets.precompile += %w( vendors/owl.carousel.min.js )
+Rails.application.config.assets.precompile += %w( vendors/jquery.sticky.js )
+Rails.application.config.assets.precompile += %w( rs-plugin/js/jquery.themepunch.tools.min.js )
+Rails.application.config.assets.precompile += %w( rs-plugin/js/jquery.themepunch.revolution.min.js )
+Rails.application.config.assets.precompile += %w( vendors/own-menu.js )
+Rails.application.config.assets.precompile += %w( vendors/jquery.isotope.min.js )
+Rails.application.config.assets.precompile += %w( zap.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
